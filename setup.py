a9ef3cfe5168e26c2b06c097950ec312e475eeaa
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-14 23:24:29 +0100

    chore(version): bump version to v0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name="nord-pygments",
-        version="0.0.1-dev.0",
+        version="0.0.1",
         url="https://github.com/lewisacidic/nord-pygments",
         author="Rich Lewis",
         author_email="opensource@richlew.is",
