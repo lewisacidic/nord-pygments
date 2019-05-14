826807532f7554589f33fb07d6ea530be283e6e9
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-14 23:22:42 +0100

    build(readme): add markdown long description

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ if __name__ == "__main__":
         author="Rich Lewis",
         author_email="opensource@richlew.is",
         description="A nord-inspired style for Pygments",
+        long_description=open('README.md').read(),
+        long_description_content_type="text/markdown",
         license="MIT",
         keywords=["pygments", "style", "nord"],
         packages=find_packages(),
