62d8aaca3900fc438079bfe46033a551d69cfba7
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-14 22:45:14 +0100

    build(setup): add url field to setup

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ if __name__ == "__main__":
     setup(
         name="nord-pygments",
         version="0.0.1-dev.0",
+        url="https://github.com/lewisacidic/nord-pygments",
         author="Rich Lewis",
-        author_email="opensource@richlew.is",
+        author_email="opensource[at]richlew.is",
         description="A nord-inspired style for Pygments",
         license="MIT",
         keywords=["pygments", "style", "nord"],
