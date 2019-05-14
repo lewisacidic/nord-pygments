40b519d310aaf0291de3de6e002ea4c8ca38f1ec
Rich Lewis <2941720+lewisacidic@users.noreply.github.com>
2019-05-14 21:43:44 +0100

    feat(package): add base package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+if __name__ == "__main__":
+    setup(
+        name="nord-pygments",
+        version="0.0.1-dev.0",
+        author="Rich Lewis",
+        author_email="opensource@richlew.is",
+        description="A nord-inspired style for Pygments",
+        license="MIT",
+        keywords=["pygments", "style", "nord"],
+        packages=find_packages(),
+    )
